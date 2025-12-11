390d71ab26a65835e2bdda08902611e69c6c36e6
James Michael Hunter Turner <michael@turneratlanta.com>
2025-12-11 15:52:51 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        custom-primary': 'oklch(0.646 0.222 41.116)',
-        'custom-accent': 'oklch(0.398 0.07 227.392)',
         'primary': 'var(--color-primary)'
       }
     }
